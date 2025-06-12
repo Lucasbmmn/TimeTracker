1851321a4094a219c3db32f913515907419821cb
Lucas <lumaga.bm@gmail.com>
2025-06-12 22:46:10 +0200

    [ProjectCommands] Modify: list -> Add list of every project, Add: add -> possibility to add project, delete -> possibility to delete a project

--- consoleApp/src/main/java/com/lucasbmmn/timetracker/console/ProjectCommands.java
+++ consoleApp/src/main/java/com/lucasbmmn/timetracker/console/ProjectCommands.java
@@ -3,13 +3,17 @@ package com.lucasbmmn.timetracker.console;
 import com.lucasbmmn.timetracker.data.dao.ProjectDao;
 import com.lucasbmmn.timetracker.model.Project;
 
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.Date;
 
 public class ProjectCommands {
     public static void executeCommand(String[] args) {
         if (args.length != 0) {
             String[] otherArgs = args.length >= 2 ? Arrays.copyOfRange(args, 1, args.length) : null;
             switch (args[0]) {
+                case "add" -> add(otherArgs);
+                case "delete" -> delete(otherArgs);
                 case "list" -> list(otherArgs);
                 default -> help();
             }
@@ -18,11 +22,37 @@ public class ProjectCommands {
         }
     }
 
-    private static void list(String[] args) {
-        if (args == null) {
-            // TODO: 10/06/2025 List every projects
-        } else if (args.length == 1) {
+    private static void add(String[] args) {
+        if (args != null &&  args.length == 1) {
+            Project project = new Project(null, args[0], "", Duration.ofSeconds(0), 0, 0,
+                    new Date(), null);
             ProjectDao dao = new ProjectDao();
+            dao.insertProject(project);
+            System.out.println("Project successfully created.");
+        }
+        else {
+            System.out.println("project list error: wrong number of parameters");
+        }
+        System.out.println();
+    }
+
+    private static void delete(String[] args) {
+        if (args != null && args.length == 1) {
+            ProjectDao dao = new ProjectDao();
+            dao.deleteProject(dao.getProjectByID(args[0]));
+            System.out.println("Project successfully deleted.");
+        }
+        else {
+            System.out.println("project list error: wrong number of parameters");
+        }
+        System.out.println();
+    }
+
+    private static void list(String[] args) {
+        ProjectDao dao = new ProjectDao();
+        if (args == null) {
+            for (Project project : dao.getAllProjects()) System.out.println(project);
+        } else if (args.length == 1) {
             Project project = dao.getProjectByID(args[0]);
             if (project != null) System.out.println(project);
             else System.out.println("project list error: no project with the id \"" + args[0] + '"');
@@ -34,6 +64,8 @@ public class ProjectCommands {
 
     private static void help() {
         System.out.println("""
+                project add <name>                                      Create a new project
+                project delete <id>                                     Delete a projet
                 project list [client-id]                                Show all projects (optionally filter by client)
                 """);
     }
