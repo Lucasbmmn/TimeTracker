35b4d259f012470988032d904df7512f3c808b5a
Lucas <lumaga.bm@gmail.com>
2025-06-14 16:11:24 +0200

    [ProjectCommands] Implement projet update command

--- consoleApp/src/main/java/com/lucasbmmn/timetracker/console/ProjectCommands.java
+++ consoleApp/src/main/java/com/lucasbmmn/timetracker/console/ProjectCommands.java
@@ -1,6 +1,8 @@
 package com.lucasbmmn.timetracker.console;
 
+import com.lucasbmmn.timetracker.data.dao.ClientDao;
 import com.lucasbmmn.timetracker.data.dao.ProjectDao;
+import com.lucasbmmn.timetracker.model.Client;
 import com.lucasbmmn.timetracker.model.Project;
 
 import java.time.Duration;
@@ -15,6 +17,7 @@ public class ProjectCommands {
                 case "add" -> add(otherArgs);
                 case "delete" -> delete(otherArgs);
                 case "list" -> list(otherArgs);
+                case "update" -> update(otherArgs);
                 default -> help();
             }
         } else {
@@ -66,11 +69,76 @@ public class ProjectCommands {
         System.out.println();
     }
 
+    private static void update(String[] args) {
+        // Update command always takes 3 parameters
+        if (args != null && args.length == 3) {
+            ProjectDao dao = new ProjectDao();
+            Project project = dao.getProjectByID(args[0]);
+
+            // Updates project only if it already exists
+            if (project != null) {
+
+                boolean isProjectModified = true;
+                // TODO: 13/06/2025 Test everything
+                switch (args[1]) {
+                    case "client", "-c" -> {
+                        Client client = (new ClientDao()).getClientByID(args[2]);
+                        if (client != null) project.setClient(client);
+                        else {
+                            isProjectModified = false;
+                            System.out.println("project update error: client does not exist");
+                        }
+                    }
+                    case "created-at", "-ca", "-cd" ->
+                            project.setCreatedAt(new Date(Long.parseLong(args[2])));
+                    case "description", "desc", "-d" -> project.setDescription(args[2]);
+                    case "deadline", "-dl" ->
+                            project.setDeadline(new Date(Long.parseLong(args[2])));
+                    case "estimated-time", "-e" ->
+                            project.setEstimatedTime(Duration.ofSeconds(Long.parseLong(args[2])));
+                    case "fixed-price", "-f" ->
+                            project.setFixedPrice(Double.parseDouble(args[2]));
+                    case "hourly-rate", "-hr" ->
+                            project.setHourlyRate(Double.parseDouble(args[2]));
+                    case "name", "-n" -> project.setName(args[2]);
+                    default -> isProjectModified = false;
+                }
+
+                if (isProjectModified) {
+                    dao.updateProject(project);
+                    System.out.println("Project successfully updated");
+                } else System.out.printf("project update error: attribute '%s' does not exist%n" +
+                        "'project update -h for more informations'%n", args[1]);
+            } else System.out.println("project update error: project does not exist");
+        } else if (args != null && (args[0].equals("-h") || args[0].equals("help"))) {
+            updateHelp();
+        } else System.out.println("project update error: wrong number of parameters");
+
+        System.out.println();
+    }
+
     private static void help() {
         System.out.println("""
                 project add <name>                                      Create a new project
                 project delete <id>                                     Delete a projet
                 project list [client-id]                                Show all projects (optionally filter by client)
+                project update <id> <attribute> <value>                 Update a project ("project update -h" for more information)
+                """);
+    }
+
+    private static void updateHelp() {
+        System.out.println("""
+                project update <id> <attribute> <value>
+                project update -h: shows this page
+                Attributes:
+                    client, -c: project's client
+                    created-at, -ca, -cd: project's creation date
+                    description, desc, -d: project's description
+                    deadline, -dl: project's deadline
+                    estimated-time, -e: project's time estimation
+                    fixed-price, -f: project's fixed price
+                    hourly-rate, -hr: project's hourly rate
+                    name, -n: project's name
                 """);
     }
 }
